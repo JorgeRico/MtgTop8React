35e47f63dce3dd77aab49c4ee2f40873fab7df97
JorgeRico <jorge.rico.garriga@gmail.com>
2025-03-22 13:13:24 +0100

    footer ads position

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,18 +11,19 @@ export default function Footer() {
     return (
         <>
             <div className="footer">
-                
-                <div className=" social mb20">
+                <div className="social mb20">
                     <LinkImage url="https://www.instagram.com/legacy.cat/" img={Instagram} />
                     <LinkImage url="https://x.com/Lliga_Legacy" img={XImg} />
                     <LinkImage url="https://www.youtube.com/@catmagiclegacy" img={CatMagic} />
                     <LinkImage url="https://www.twitch.tv/catmagiclegacy" img={Twitch} />                    
                 </div>
-                <div className=" message">
+                <div className="message">
                     <GoogleAd slot="6035410401" googleAdId="ca-pub-9482818665347681"/>
+                </div>
+                <div className="message mt10">
                     The literal and graphical information presented on this site about Magic: The Gathering, including card images and mana symbols, is copyright Wizards of the Coast, LLC. Scryfall is not produced by or endorsed by Wizards of the Coast.
                 </div>
-                <div className=" message mt10">Ugly design rules!!!</div>
+                <div className="message mt10">Ugly design rules!!!</div>
             </div>
         </>
     );
