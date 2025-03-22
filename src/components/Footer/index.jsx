97beec53fd3fbb447dbeb40cb66b5c1ba4fd70a8
JorgeRico <jorge.rico.garriga@gmail.com>
2025-03-22 18:37:14 +0100

    try ads mobile

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,8 @@ import CatMagic from "/src/assets/images/youtube.png";
 import Twitch from "/src/assets/images/twitch.png";
 import LinkImage from "/src/components/Link/Image";
 import "./module.css";
-import GoogleAd from '/src/components/Ads';            
+// import GoogleAd from '/src/components/Ads';
+import GoogleAdMobile from '/src/components/Ads/Mobile';
 
 export default function Footer() {
     return (
@@ -18,7 +19,8 @@ export default function Footer() {
                     <LinkImage url="https://www.twitch.tv/catmagiclegacy" img={Twitch} />                    
                 </div>
                 <div className="message">
-                    <GoogleAd slot="6035410401" googleAdId="ca-pub-9482818665347681"/>
+                    {/* <GoogleAd slot="6035410401" googleAdId="ca-pub-9482818665347681"/> */}
+                    <GoogleAdMobile slot="3978326343" googleAdId="ca-pub-9482818665347681"/>
                 </div>
                 <div className="message mt10">
                     The literal and graphical information presented on this site about Magic: The Gathering, including card images and mana symbols, is copyright Wizards of the Coast, LLC. Scryfall is not produced by or endorsed by Wizards of the Coast.
