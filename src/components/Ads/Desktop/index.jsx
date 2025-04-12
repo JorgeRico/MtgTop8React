41b23da1e2e1d0e8ad8a13f6a4872b12bdf019e4
JorgeRico <jorge.rico.garriga@gmail.com>
2025-04-13 00:59:26 +0200

    ads

--- src/components/Ads/Desktop/index.jsx
+++ src/components/Ads/Desktop/index.jsx
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 class GoogleAd extends Component {
-    // googleInit = null;
-
-    // componentDidMount() {
-    //     const { timeout } = this.props;
-    //     this.googleInit = setTimeout(() => {
-    //         if (typeof window !== 'undefined')
-    //             (window.adsbygoogle = window.adsbygoogle || []).push({});
-    //     }, timeout);
-    // }
-
-    // componentWillUnmount() {
-    //     if (this.googleInit) clearTimeout(this.googleInit);
-    // }
-
     render() {
         const { classNames, slot, googleAdId } = this.props;
         return (
             <div className={classNames}>
                 <ins
                 className="adsbygoogle"
-                style={{display: 'inline-block', textAlign: "center", width: '728px', height: '90px'}}
+                style={{display: 'block'}}
                 data-ad-client={googleAdId}
                 data-ad-slot={slot}
+                data-ad-format="auto"
+                data-full-width-responsive="true"
                 ></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
