bc0dd860f337e4456463a8f554e129ed60e69e9a
JorgeRico <jorge.rico.garriga@gmail.com>
2025-04-13 22:28:28 +0200

    ads

--- src/components/Ads/Desktop/index.jsx
+++ src/components/Ads/Desktop/index.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 
 function GoogleAd(props) {
     useEffect(() => {
-            window.adsbygoogle = window.adsbygoogle || []
-            window.adsbygoogle.push({})
+            (adsbygoogle = window.adsbygoogle || []).push({});
     }, []);
 
     return (
@@ -14,7 +13,6 @@ function GoogleAd(props) {
                 style={{display: 'inline-block', textAlign: "center", width: '728px', height: '90px'}}
                 data-ad-client={props.googleAdId}
                 data-ad-slot={props.slot}
-                data-full-width-responsive="true"
             ></ins>
         </div>
     );
