a1662ec295210799c77ecdc33f4face457831e6e
JorgeRico <jorge.rico.garriga@gmail.com>
2025-04-18 19:28:34 +0200

    delete comment

--- src/components/List/Deck/index.jsx
+++ src/components/List/Deck/index.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import "./module.css";
 import statsTypes from "/src/services/statsTypes.js"
 
-// TODO: component needs refactor
 export default function Deck(props) {
     const { items }                                   = props;
     const effectRan                                   = useRef(false);
