e4d56e762350d27f494fcf943c2c26391f6ab373
JorgeRico <jorge.rico.garriga@gmail.com>
2025-04-22 18:34:13 +0200

    player list refactor

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -4,64 +4,22 @@ import PropTypes from 'prop-types';
 import Deck from "/src/components/List/Deck";
 import BluredBigList from "/src/components/Blured/FakeLists/BigList";
 import "./module.css";
-import { useApi } from '/src/hooks/use-api.js';
-import endpoints from "/src/services/endpoints.js"
+import PlayerItem from "/src/components/Player/Item";
 
 export default function StatsPlayer(props) {
     const { items }                               = props;
     const effectRan                               = useRef(false);
     const [ renderItems, setRenderItems ]         = useState([]);
     const [ renderDeckItems, setRenderDeckItems ] = useState([]);
-    const [ showSpinner, setShowSpinner ]         = useState(false);
-    const api                                     = useApi();
+    const [ loading, setLoading ]                 = useState(false);
 
     StatsPlayer.propTypes = {
         items : PropTypes.array
     };
 
-    // api call
-    async function apiCall(id) {
-        await api.getAxiosEndpoint(endpoints.API_DECK_CARDS.replace('{id}', id))
-        .then((response) => {
-            setTimeout(() => {setShowSpinner(false)}, 1000);
-            setTimeout(() => {setRenderDeckItems(response.data)}, 1000);
-        })
-        .catch((err) => { 
-            console.log('error loading deck')
-        });
-    }
-
-    function handleCards(index, idDeck) {
-        setRenderDeckItems(null)
-        apiCall(idDeck)
-        playersUnselected();
-        playerSelected(index);
-        setShowSpinner(true);
-    }
-
-    function playersUnselected() {
-        for (var i = 0; i < items.length; i++) {
-            document.querySelector('#player-' + (i+1)).classList.remove('color-selected');
-        }
-    }
-    
-    function playerSelected(index) {
-        document.querySelector('#player-' + index).classList.add('color-selected');
-    }
-
     useEffect(() => {
         if (!effectRan.current) {
-            setRenderItems(items?.map((item, index) => (
-                <li key={uuidv4()} onClick={() => handleCards(index+1, item.idDeck)} id={'player-'+(index+1)} className="listItem pointer deckNameList">
-                    <div className="left line w100">
-                        <div className="left line ">{item.name}</div>
-                    </div>
-                    <div className="left line w100 ml15 mt10">
-                        <div className="circle blueCircle"></div>
-                        <div className="left line">{item.deckName}</div>
-                    </div>
-                </li>   
-            )));
+            setRenderItems(items)
         }
         
         return () => effectRan.current = true;
@@ -73,12 +31,14 @@ export default function StatsPlayer(props) {
             <div className="left ml5">
                 <ol>
                     {(items.length > 0) && (
-                        renderItems
+                        items.map((item, index) => (
+                            <PlayerItem item={item} index={index} setLoading={setLoading} setRenderDeckItems={setRenderDeckItems} key={uuidv4()}></PlayerItem>
+                        ))
                     )}
                 </ol>
             </div>
             <div className="left w70 cards">
-                {showSpinner === true &&
+                {loading === true &&
                     <div className="bluredDeck">
                         <BluredBigList></BluredBigList>
                     </div>
