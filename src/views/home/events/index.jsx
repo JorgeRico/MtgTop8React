edcc899e674b04b2b135647dc446c6c14769de43
JorgeRico <jorge.rico.garriga@gmail.com>
2025-04-22 12:56:08 +0200

    home refactor components

---
+++ src/views/home/events/index.jsx
@@ -0,0 +1,53 @@
+import React, { useRef, useState, useEffect } from "react";
+import endpoints from "/src/services/endpoints.js"
+import { useApi } from '/src/hooks/use-api.js';
+import ListLeague from "/src/components/List/League";
+import BluredSmallList from "/src/components/Blured/FakeLists/SmallList";
+import Title from "components/HTag/Title";
+
+function Events(props) {
+    const api                                  = useApi();
+    const effectRan                            = useRef(false);
+    const [ renderElements, setRenderElements] = useState(null);
+    const [ showElements, setShowElements ]    = useState(true);
+    const { title, endpoint }                  = props;
+
+    // api call
+    async function apiCall() {
+        await api.getAxiosEndpoint(endpoint)
+        .then((response) => {
+            setRenderElements(response.data);
+            setShowElements(false);
+        })
+        .catch((err) => { 
+            console.log('Error' + title)
+        });
+    }
+
+    useEffect(() => {
+        if (!effectRan.current) {
+            apiCall();
+        }
+        
+        return () => effectRan.current = true;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [renderElements]);
+
+    return (
+        <>
+            <Title title={title} />
+            {showElements === true ? (
+                    <BluredSmallList></BluredSmallList>
+                ) : (
+                    <>
+                        {renderElements != null && (
+                            <ListLeague url={endpoints.API_LEAGUE} items={renderElements} />
+                        )}
+                    </>
+                )
+            }
+        </>
+    );
+}
+
+export default Events;
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,78 +1,15 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import endpoints from "/src/services/endpoints.js"
-import { useApi } from '/src/hooks/use-api.js';
-import ListLeague from "/src/components/List/League";
-import BluredSmallList from "/src/components/Blured/FakeLists/SmallList";
 import Layout from "/src/views/layout";
-import Title from "components/HTag/Title";
+import Events from "/src/views/home/events";
 
 function Home() {
-    const api                                             = useApi();
-    const effectRan                                       = useRef(false);
-    const [ renderElements, setRenderElements]            = useState(null);
-    const [ renderPastElements, setRenderPastElements]    = useState(null);
-    const [ waitingElements, setWaitingElements ]         = useState(true);
-    const [ waitingPastElements, setWaitingPastElements ] = useState(true);
-
-    // api call
-    async function apiCurrentCall() {
-        await api.getAxiosEndpoint(endpoints.API_LEAGUE_CURRENT)
-        .then((response) => {
-            setRenderElements(response.data);
-            setWaitingElements(false);
-        })
-        .catch((err) => { 
-            console.log('error current tournaments')
-        });
-    }
-
-    // api call
-    async function apiPastCall() {
-        await api.getAxiosEndpoint(endpoints.API_LEAGUE_PAST)
-        .then((response) => {
-            setRenderPastElements(response.data);
-            setWaitingPastElements(false);
-        })
-        .catch((err) => { 
-            console.log('error past tournaments')
-        });
-    }
-
-    useEffect(() => {
-        if (!effectRan.current) {
-            apiCurrentCall();
-            apiPastCall();
-        }
-        
-        return () => effectRan.current = true;
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [renderElements]);
-
-    const showElements = (waitingElements, renderElements) => {
-        return (
-            <>
-                {waitingElements === true ? (
-                        <BluredSmallList></BluredSmallList>
-                    ) : (
-                        <>
-                            {renderElements != null && (
-                                <ListLeague url={endpoints.API_LEAGUE} items={renderElements} />
-                            )}
-                        </>
-                    )
-                }
-            </>
-        )
-    }
-
     return (
         <>
             <Layout name="home">
                 <div className="left w100 mt20">
-                    <Title title="Current Events" />
-                    {showElements(waitingElements, renderElements)}
-                    <Title title="Past Events" />
-                    {showElements(waitingPastElements, renderPastElements)}
+                    <Events endpoint={endpoints.API_LEAGUE_CURRENT} title="Current Events"></Events>
+                    <Events endpoint={endpoints.API_LEAGUE_PAST} title="Past Events"></Events>
                 </div>
             </Layout>
         </>
