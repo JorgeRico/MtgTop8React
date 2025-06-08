fabdf65cce33c0cba1750619a5145a495f5a56cb
JorgeRico <jorge.rico.garriga@gmail.com>
2025-06-08 23:54:31 +0200

    hotfix cookies reloads

--- src/views/layout/core/index.jsx
+++ src/views/layout/core/index.jsx
@@ -2,27 +2,18 @@ import PropTypes from "prop-types";
 import Header from "/src/components/Header";
 import Footer from "/src/components/Footer";
 import React, { useRef, useEffect } from "react";
-import { Analytics } from "@vercel/analytics/react";
-    
-
+import { Analytics } from "@vercel/analytics/react"
 function Layout(props) {
-    const effectRan = useRef(false);
-    
-     useEffect(() => {
-            if (!effectRan.current) {
-                // load cookie banner
-                if (import.meta.env.VITE_APP_ENV === 'production') {
-                    const script = document.createElement('script');
-                    script.id    = "cookieyes"
-                    script.src   = 'https://cdn-cookieyes.com/client_data/fa039951ac4a85e556944902/script.js';
-                    script.async = true;
-                    document.body.appendChild(script);
-                }
-            }
-            
-            return () => effectRan.current = true;
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []);
+    // const effectRan = useRef(false);
+
+    // useEffect(() => {
+    //     if (!effectRan.current) {
+    //         var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    //     }
+        
+    //     return () => effectRan.current = true;
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
 
     return (
         (
