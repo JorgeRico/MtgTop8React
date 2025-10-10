53bf91aa025f9b00440928a17914b6b7d4daaf22
JorgeRico <jorge.rico.garriga@gmail.com>
2025-10-10 04:46:13 +0200

    hotfix missing stats header style

--- src/components/List/Stats/Block/Header/index.jsx
+++ src/components/List/Stats/Block/Header/index.jsx
@@ -10,7 +10,7 @@ export default function TopStatsList(props) {
     const topPlayerStats = () => {
         return (
             <>
-                <div className="cardItem overflowHidden">
+                <div className="cardItem overflowHidden bg-footer p5 mb10">
                     <span className="left ml25">Total</span>
                     <span className="left ml15">Name</span>
                 </div>
@@ -21,7 +21,7 @@ export default function TopStatsList(props) {
     const topCardStats = () => {
         return (
             <>
-                <div className="cardItem overflowHidden">
+                <div className="cardItem overflowHidden bg-footer p5 mb10">
                     <span className="left ml15 w-15">&nbsp;</span>
                     <span className="left ml15">Total</span>
                     <span className="left ml15">Name</span>
