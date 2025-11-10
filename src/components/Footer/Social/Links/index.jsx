2553fe5d52c0b8fdc5118e3349896b754b6da9ff
JorgeRico <jorge.rico.garriga@gmail.com>
2025-11-10 03:28:50 +0100

    hotfix: small seo issues

--- src/components/Footer/Social/Links/index.jsx
+++ src/components/Footer/Social/Links/index.jsx
@@ -8,10 +8,10 @@ export default function SocialLinksFooter() {
     return (
         <>
             <div className="left w100 links mb15">
-                <LinkImage url="https://www.instagram.com/legacy.cat/" img={Instagram} />
-                <LinkImage className="ml15" url="https://x.com/Lliga_Legacy" img={XImg} />
-                <LinkImage className="ml15" url="https://www.youtube.com/@catmagiclegacy" img={Youtube} />
-                <LinkImage className="ml15" url="https://www.twitch.tv/catmagiclegacy" img={Twitch} />         
+                <LinkImage url="https://www.instagram.com/legacy.cat/" img={Instagram} title="instagram legacy.cat"/>
+                <LinkImage className="ml15" url="https://x.com/Lliga_Legacy" img={XImg} title="x legacy.cat"/>
+                <LinkImage className="ml15" url="https://www.youtube.com/@catmagiclegacy" img={Youtube} title="catmagiclegacy youtube channel"/>
+                <LinkImage className="ml15" url="https://www.twitch.tv/catmagiclegacy" img={Twitch} title="catmagiclegacy twitch channel"/>
             </div>
         </>
     );
--- src/components/Logo/Images/index.jsx
+++ src/components/Logo/Images/index.jsx
@@ -4,7 +4,7 @@ import Logos from "/src/assets/images/logos.png";
 export default function LogoImage() {
     return (
         <>
-            <img src={Logos} alt="mtg stats" className="f24 logos invertColor" />
+            <img src={Logos} width="100" height="17" alt="mtg stats" className="f24 logos invertColor" />
         </>
     );
 }
