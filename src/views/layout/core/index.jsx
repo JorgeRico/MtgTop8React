95df46aed7e9e1072f45f889d92363f6e26c30f0
JorgeRico <jorge.rico.garriga@gmail.com>
2026-01-30 23:37:01 +0100

    feature: google adsense tag

--- src/views/layout/core/index.jsx
+++ src/views/layout/core/index.jsx
@@ -18,6 +18,18 @@ function Layout(props) {
                         </div>
                     </div>
                 </div>
+                <div className={`overflowHidden container ${props.name}`}>
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9482818665347681" crossOrigin="anonymous"></script>
+                    <ins className="adsbygoogle"
+                        style={{display:'block'}}
+                        data-ad-client="ca-pub-9482818665347681"
+                        data-ad-slot="7691872894"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>    
+                </div>
                 <Footer></Footer>
                 <Analytics></Analytics>
             </>
