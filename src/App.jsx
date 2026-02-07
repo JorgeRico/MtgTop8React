1c870ec8397d9eec56b949f501a19afc221ff0b1
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-07 03:10:45 +0100

    feature: upgrade packages

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
     return (
-        <BrowserRouter
-            future={{
-                v7_startTransition: true,
-                v7_relativeSplatPath: true,
-            }}
-        >
+        <BrowserRouter>
             <CheckBotProvider>
                 <ApiProvider>
                     <Router></Router>
