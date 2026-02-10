b9033d2556840dc927d3d48eae15f16374bd7914
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-10 02:54:00 +0100

    feature: lazy load routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,20 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from "../views/home";
-import League from "../views/league";
-import Tournament from "../views/tournament";
-import Contact from "../views/contact";
-import Decklist from "../views/decklist";
-import NotFound from "../views/notfound";
+import { lazy, Suspense } from 'react';
+const Home       = lazy(() => import("../views/home"));
+const League     = lazy(() => import("../views/league"));
+const Tournament = lazy(() => import("../views/tournament"));
+const Contact    = lazy(() => import("../views/contact"));
+const Decklist   = lazy(() => import("../views/decklist"));
+const NotFound   = lazy(() => import("../views/notfound"));
 
 const Router = () => (  
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="leagues/:id" element={<League />} />
-        <Route path="tournaments/:id" element={<Tournament />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="decklist" element={<Decklist />} />
-        <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<p>Loading . . .</p>}>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="leagues/:id" element={<League />} />
+            <Route path="tournaments/:id" element={<Tournament />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="decklist" element={<Decklist />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </Suspense>
 );
 
 export default Router;
