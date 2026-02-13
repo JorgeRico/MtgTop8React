daf5775aa5b4c29772686a195d8b778d3490df74
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-13 12:19:04 +0100

    hotfix: google ads deprecated

--- src/components/Adsense/index.jsx
+++ src/components/Adsense/index.jsx
@@ -1,42 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
  
-const AdSenseAd = ({ adClient, adSlot, adFormat = "auto", fullWidthResponsive = true, adClassNameStyles = "" }) => {
+const AdSenseAd = ({ adClient, adSlot, adFormat = "rspv", fullWidthResponsive = true, adClassNameStyles = "" }) => {
     const scriptLoaded   = useRef(false);
     const adContainerRef = useRef(null);
     
     // Load AdSense script (once)
-    useEffect(() => {
-        if (!scriptLoaded.current) {
-            const script = document.createElement('script');
-            script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adClient}`;
-            script.crossOrigin = 'anonymous';
-            script.async = true;
-            document.head.appendChild(script);
-            scriptLoaded.current = true;
-        }
-    }, [adClient]);
-    
-    // Initialize the ad after the container is rendered
-    useEffect(() => {
-        if (adContainerRef.current && window.adsbygoogle) {
-            // Push an empty object to trigger ad rendering
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        }
-    }, [adSlot]); // Re-run if adSlot changes (e.g., different ad units)
+    // useEffect(() => {
+    //     if (!scriptLoaded.current) {
+    //         const script = document.createElement('script');
+    //         script.src = `https://cdn.ampproject.org/v0/amp-ad-0.1.js`;
+    //         script.customElement = 'amp-ad';
+    //         script.async = true;
+    //         document.head.appendChild(script);
+    //         scriptLoaded.current = true;
+    //     }
+    // }, [adClient]);
     
     return (
         <div className={`left w100 ${adClassNameStyles} ad-container`}>
-            <ins 
+            <amp-ad width="100vw" height="320"
                 ref={adContainerRef} // Attach ref to track the container
-                className="adsbygoogle"
-                style={{ display: 'block', width: '100%', minWidth: '300px' }} // Ensure the ad takes full width and has a minimum width
-                data-ad-client={adClient}
+                type="adsense"
+                ddata-ad-client={adClient}
                 data-ad-slot={adSlot}
                 data-ad-format={adFormat}
-                data-full-width-responsive={fullWidthResponsive}
-                // data-adtest="on"
-            />
+                data-full-width="">
+                <div overflow=""></div>
+            </amp-ad>    
         </div>
+        
     );
 };
  
