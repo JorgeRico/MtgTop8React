5f33ec07290a014fb1e109654e3aef04b7999720
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-13 12:26:06 +0100

    hotfix: header tag

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 import LogoImage from "/src/components/Logo/Images";
 import endpoints from "/src/services/endpoints.jsx";
-import HTag from "/src/components/HTag";
 
 export default function Logo() {
     return (
         <>
             <Link to={endpoints.API_HOME}>
-                <HTag Tag="h5" className="f24" text="Stats" />
+                <p className="f24 mb10 mt0">Stats</p>
                 <LogoImage />
             </Link>
         </>
