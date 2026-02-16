fff8dbcac50913af9b5c6aafcfebc65421f3f270
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-17 00:59:43 +0100

    feature: footer languages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import RightBlock from "/src/components/Header/RightBlock";
 import Mail from "/src/assets/images/mail.png";
 import Cards from "/src/assets/images/cards.png";
 import endpoints from "/src/services/endpoints.jsx";
-import Language from "/src/components/Language";
+// import Language from "/src/components/Language";
 import { useTranslation } from 'react-i18next';
 
 export default function Header({ classNameStyles }) {
@@ -14,7 +14,7 @@ export default function Header({ classNameStyles }) {
             <section className={classNameStyles}>
                 <article className="left w100 overflowHidden header">
                     <HeaderLogo></HeaderLogo>
-                    <Language></Language>
+                    {/* <Language></Language> */}
                     <RightBlock image={Mail} text={t('header.Contact us')} endpoint={endpoints.HTTP_CONTACT} styleClasses="mr20"></RightBlock>
                     <RightBlock image={Cards} text={t('header.Decklist')} endpoint={endpoints.HTTP_DECKLIST}></RightBlock>
                 </article>
--- src/components/Language/index.jsx
+++ src/components/Language/index.jsx
@@ -1,21 +1,7 @@
 import { useTranslation } from "react-i18next";
 import "./module.css";
 import { useRef } from 'react';
-
-const languages = [
-    { 
-        code: "en", 
-        name: "English"
-    },
-    { 
-        code: "es", 
-        name: "Español"
-    },
-    { 
-        code: "cat", 
-        name: "Català"
-    },
-];
+import languages from "/src/services/languages.jsx";
 
 function LanguageSwitcher() {
     const { i18n }         = useTranslation();
@@ -40,27 +26,25 @@ function LanguageSwitcher() {
 
     return (
         <>
-            <div className="" >
-                <div className="languageBox">
-                    <button className="bg-footer" onClick={() => handleClickButton()}>
-                        <span className="uppercase">{i18n.language}</span>
+            <div className="languageBox">
+                <button className="bg-footer" onClick={() => handleClickButton()}>
+                    <span className="uppercase">{i18n.language}</span>
+                    <span> - </span>
+                    <span>{getLangInfo(i18n.language)}</span>
+                </button>
+            </div>
+            <div className="languageBox bg-footer none mt10" ref={langContainerRef}>
+                {languages.map((lang) => (
+                    <button
+                        className={`${i18n.language === lang.code && "selected"}`}
+                        key={lang.code}
+                        onClick={() => changeLanguage(lang.code)}
+                    >
+                        <span className="uppercase">{lang.code}</span>
                         <span> - </span>
-                        <span>{getLangInfo(i18n.language)}</span>
+                        <span>{lang.name}</span>
                     </button>
-                </div>
-                <div className="languageBox bg-footer none mt10" ref={langContainerRef}>
-                    {languages.map((lang) => (
-                        <button
-                            className={`${i18n.language === lang.code && "selected"}`}
-                            key={lang.code}
-                            onClick={() => changeLanguage(lang.code)}
-                        >
-                            <span className="uppercase">{lang.code}</span>
-                            <span> - </span>
-                            <span>{lang.name}</span>
-                        </button>
-                    ))}
-                </div>
+                ))}
             </div>
         </>
     );
