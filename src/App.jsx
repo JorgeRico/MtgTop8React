2d92fb968c743a1a855bfff6729a728d72f53002
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-25 14:09:47 +0100

    hotfix: disable check bot context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { CheckBotProvider } from './context/bot-context';
+// import { CheckBotProvider } from './context/bot-context';
 import Router from "./router/routes.jsx";
 import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
     return (
         <BrowserRouter>
-            <CheckBotProvider>
-                <Router></Router>
-            </CheckBotProvider>
+            <Router></Router>
         </BrowserRouter>
     );
 }
